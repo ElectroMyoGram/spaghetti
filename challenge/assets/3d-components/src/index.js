f102715a514aed5d55f42ff2305d1b3c7a0a26ff
Ethan <e.gilbert@whitgift.co.uk>
2024-04-22 16:50:25 +0100

    earth now rotates :)

--- challenge/assets/3d-components/src/index.js
+++ challenge/assets/3d-components/src/index.js
@@ -1,6 +1,13 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+
+const earth_axis_of_rotation = 23.5;
+const ROTATION_SPEED = 0.0001;
+const LINE_COLOUR = 0xffff00;
+
+
+
 function initThreeScene(){
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -13,15 +20,24 @@ function initThreeScene(){
 
     const sphere_geometry = new THREE.SphereGeometry(1);
     const material = new THREE.MeshBasicMaterial( { map: sphere_texture } );
-    const sphere = new THREE.Mesh(sphere_geometry, material)
-    scene.add( sphere );
+    const earth = new THREE.Mesh(sphere_geometry, material)
+    scene.add( earth );
 
     camera.position.z = 5;
     const controls = new OrbitControls( camera, renderer.domElement );
     controls.update();
+
+
+    let earth_axis = new THREE.Vector3(1, Math.tan(earth_axis_of_rotation), 0);
+
+    const arrowHelper = new THREE.ArrowHelper(earth_axis.clone().normalize(), new THREE.Vector3(0, 0, 0), 10, LINE_COLOUR);
+
+
+    scene.add(arrowHelper);
     function animate() {
         requestAnimationFrame( animate );
-
+        
+        earth.rotateOnAxis(earth_axis, ROTATION_SPEED);
 
         renderer.render( scene, camera );
     }
